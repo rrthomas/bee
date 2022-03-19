bc2f43b7fa7ee3eccca75b5eab7a4bb2b71506d3
Reuben Thomas <rrt@sc3d.org>
2022-03-19 07:04:04 +0000

    traps.c: #include <stdio.h> for remove(3)

--- src/traps.c
+++ src/traps.c
@@ -1,6 +1,6 @@
 // The traps.
 //
-// (c) Reuben Thomas 1994-2020
+// (c) Reuben Thomas 1994-2022
 //
 // The package is distributed under the GNU General Public License version 3,
 // or, at your option, any later version.
@@ -13,6 +13,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
